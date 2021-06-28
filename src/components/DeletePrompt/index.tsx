7228451505af85174e04f41401499baa3524690b
Matheus Mitestainer <matheusmitestainer@outlook.com>
2021-06-27 22:25:16 -0300

    Add prompt to delete question

---
+++ src/components/DeletePrompt/index.tsx
@@ -0,0 +1,30 @@
+import { Overlay, Prompt, TextWrapper, ButtonsWrapper } from './styles'
+import { Button } from '../Button'
+import { FiXCircle } from 'react-icons/fi'
+
+type PromptProps = {
+  handler: () => void
+  exit: () => void
+}
+
+export const DeletePrompt: React.FC<PromptProps> = ({ handler, exit }) => {
+  return (
+    <Overlay>
+      <Prompt>
+        <FiXCircle size={48} color="#E73F5D" />
+        <TextWrapper>
+          <strong>Encerrar sala</strong>
+          <p>Tem certeza que você deseja encerrar esta sala?</p>
+        </TextWrapper>
+        <ButtonsWrapper>
+          <Button onClick={exit}>
+            Cancelar
+          </Button>
+          <Button onClick={handler}>
+            Sim, encerrar
+          </Button>
+        </ButtonsWrapper>
+      </Prompt>
+    </Overlay>
+  )
+}
